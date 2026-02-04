199293b52fcd8cf6b3e5feec2b62bf941994147a
Carlos Salas Alarcón <110690378+csalas-alarcon@users.noreply.github.com>
2026-02-04 12:41:14 +0100

    Some rearranging

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
-python -m venv .venv
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+fi
+
 source .venv/bin/activate
-pip install numpy matplotlib networkx scipy pillow ipykernel
+
+pip install -r requirements.txt
 
 python -m ipykernel install --user --name=myenv --display-name "Python (My Project Env)"
